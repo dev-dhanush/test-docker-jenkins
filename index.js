237aededc35455439477f77e53614176493ad6e3
Dhanasekar <135938921+dev-dhanush@users.noreply.github.com>
2025-06-28 16:08:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
   res.end("Hello from Docker!");
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(5000, '0.0.0.0', () => {
+  console.log("Server running on port 5000");
 });
